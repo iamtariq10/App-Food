f68b25b6bdb096c2b04d8e2cb9defa63aaea0e57
Mohammad Tariq <m.faixal16212@gmail.com>
2021-08-22 18:16:14 +0500

    Food App

---
+++ Components/Navbar.js
@@ -0,0 +1,41 @@
+import logo from '../Images/logo4.png'
+function Navbar() {
+    
+
+
+return(
+
+<>
+
+<head className="head-flex">
+<h2 className='logoHeading'>FoodStreet</h2>
+
+
+<img  className='logo' src={logo} />
+
+<ul className='ul'>
+
+    <li > <a> Home </a></li>
+    <li><a href='#footer'>  About </a></li>
+    <li><a> Services </a></li>
+    <li><a> Contact </a></li>
+     <li><a href='#Menu'> Menu/Order</a></li>
+    <li ><a> Sign Up </a></li>
+
+</ul>
+
+</head>
+
+
+</>
+
+
+)
+
+
+
+
+}
+
+
+export default Navbar;
